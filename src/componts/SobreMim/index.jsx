57f747a7587df057c0347fa570c2faaec986d8da
KauanArrudaMachado380 <152728989+KauanArrudaMachado380@users.noreply.github.com>
2025-01-15 11:28:36 -0300

    banner finalizado e sobre mim inicializado

--- src/componts/SobreMim/index.jsx
+++ src/componts/SobreMim/index.jsx
@@ -1,7 +1,15 @@
+import styled from "./SobreMim.module.css";
+import fotoMinha from "../../assets/foto-de-perfil.jpg";
+
+
 function SobreMim() {
     return(
-        <article>
-            <h1>Sobre mim!</h1>
+        <article className={styled.article}>
+            <div>
+                <h1></h1>
+                <p></p>
+            </div>
+            <img src={fotoMinha} alt='Foto do Kauan' />
         </article>
     )
 }

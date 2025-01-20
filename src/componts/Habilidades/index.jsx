efe6b7c999d33c6c38f31076ed805bceca515d66
KauanArrudaMachado380 <152728989+KauanArrudaMachado380@users.noreply.github.com>
2025-01-20 14:39:19 -0300

    habilidades terminado, começando projetos

--- src/componts/Habilidades/index.jsx
+++ src/componts/Habilidades/index.jsx
@@ -1,18 +1,18 @@
-import styled from './Habilidades.module.css';
 import BR from 'country-flag-icons/react/3x2/BR';
 import US from 'country-flag-icons/react/3x2/US';
 import ES from 'country-flag-icons/react/3x2/ES';
+import styled from './Habilidades.module.css';
 
 const frontEnd =
     <div className={styled.cardFront}>
         <h2>Font-end</h2>
         <ul>
-            <li>HTML & CSS</li>
-            <li>Bootstrap</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Router-React</li>
-            <li>Module styles</li>
+            <li>Criação de interfaces com HTML e CSS</li>
+            <li>Estilização avançada com Bootstrap</li>
+            <li>Desenvolvimento dinâmico com JavaScript</li>
+            <li>Construção de SPAs com React</li>
+            <li>Gerenciamento de rotas com React Router</li>
+            <li>Estilização escopada com CSS Modules</li>
         </ul>
     </div>;
 
@@ -20,18 +20,30 @@ const linguagem =
     <div className={styled.cardLinguagem}>
         <h2>Idiomas</h2>
         <ul>
-            <li>Português (nativo) <BR title='Brasil' classname={styled.brasil}/></li>
-            <li>Inglês (B2) <US title='Estados Unidos' classname={styled.usa}/></li>
-            <li>Espanhol (A2) <ES title='Espanha' classname={styled.espanha}/></li>
+            <li>Português (nativo) 
+                <span>
+                    <BR title="Brasil" className={styled.brasil} />
+                </span>
+            </li>
+            <li>Inglês (B1 - Intermediário) 
+                <span>
+                    <US title="Estados Unidos" className={styled.usa} />
+                </span>
+            </li>
+            <li>Espanhol (A2 - Básico) 
+                <span>
+                    <ES title="Espanha" className={styled.espanha} />
+                </span>
+            </li>
         </ul>
     </div>;
 
 const UiUx = 
     <div className={styled.cardUiUx}>
-        <h2>UI/UX</h2>
+        <h2>Ui/Ux</h2>
         <ul>
             <li>Utilização do Figma para criar protótipos interativos e analisar layouts de UI/UX</li>
-            <li>Habilidade em desenvolver interfaces responsivas utilizando <strong>Grid</strong> e <strong>Flexbox</strong>, garantindo uma boa experiência em diferentes dispositivos</li>
+            <li>Habilidade em desenvolver interfaces responsivas utilizando <strong>Grid</strong> e <strong>Flexbox</strong>.</li>
             <li>Implementação de boas práticas de UX, como navegação intuitiva</li>
         </ul>
     </div>;
@@ -40,9 +52,11 @@ const outras =
     <div className={styled.cardOutros}>
         <h2>Outros</h2>
         <ul>
-            <li>Git</li>
-            <li>Node.js</li>
-            <li>JSON.js</li>
+            <li>Controle de versões com Git</li>
+            <li>Desenvolvimento com Node.js</li>
+            <li>Manipulação de dados com JSON</li>
+            <li>Edição básica de imagens</li>
+            <li>Adaptabilidade e aprendizado rápido</li>
         </ul>
     </div>;
 
@@ -51,7 +65,7 @@ const listaHabilidades = [frontEnd, linguagem, UiUx, outras];
 function Habilidades () {
     return (
         <section className={styled.section}>
-            <h1>Habilidades</h1>
+            <h1>Minhas Habilidades</h1>
             <div className={styled.containerCards}>
                 {listaHabilidades.map((item) => {return(
                     item
--- src/componts/SobreMim/index.jsx
+++ src/componts/SobreMim/index.jsx
@@ -5,11 +5,14 @@ import fotoMinha from "../../assets/fotoPerfil.jpg";
 function SobreMim() {
     return(
         <article className={styled.article}>
-            <div className={styled.card}>
-                <p>Olá! Meu nome é Kauan Arruda Machado, sou estudante de Análise e Desenvolvimento de Sistemas pela Universidade Paulista (Unip) e aspirante a desenvolvedor Front-end.
-                Sou apaixonado por tecnologia e gosto de resolver problemas. Estou sempre em busca de aprimorar minhas habilidades, explorar novas ferramentas e me manter atualizado com as tendências da área de desenvolvimento. </p>
+            <h1>Sobre mim</h1>
+            <div className={styled.container}>
+                <div className={styled.card}>
+                    <p>Olá! Meu nome é Kauan Arruda Machado, sou estudante de Análise e Desenvolvimento de Sistemas pela Universidade Paulista (Unip) e aspirante a desenvolvedor Front-end.
+                    Sou apaixonado por tecnologia e gosto de resolver problemas. Estou sempre em busca de aprimorar minhas habilidades, explorar novas ferramentas e me manter atualizado com as tendências da área de desenvolvimento. </p>
+                </div>
+                <img src={fotoMinha} alt='Foto do Kauan' />
             </div>
-            <img src={fotoMinha} alt='Foto do Kauan' />
         </article>
     )
 }
